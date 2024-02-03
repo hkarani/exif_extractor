089b751d15e7bae0a1b46e952839741b0fb1cc2f
hkarani <hezronkaranii@gmail.com>
2024-02-04 00:35:53 +0300

    Get file path name

--- test_run.py
+++ test_run.py
@@ -1,8 +1,27 @@
 import base64
 import requests
+import re
 
-image_file = open('/DSCN0010.jpg','rb').read()
-base64data = base64.b64encode(image_file).decode("utf8")
-payload = {'image': base64data}
-r = requests.post("http://localhost:9000/2015-03-31/functions/function/invocations",json=payload)
-print(r.json())
+def extract_exif_data(file_path):
+    ###
+    # The image passed in the file path is converted into base64
+    # and sent to the server as a binary payload, the filename is also
+    # sent for reconstrution on the server side
+    ###
+    image_file = open(file_path,'rb').read()
+    base64data = base64.b64encode(image_file).decode("utf8")
+    file_name = get_file_name(file_path)
+    payload = {'image': base64data, 'file_path': file_name}
+    r = requests.post("http://localhost:9000/2015-03-31/functions/function/invocations",json=payload)
+    print(r.json())
+    return r.json()
+
+def get_file_name(file_path):
+    # Extract file name from the path
+    match = re.search(r'[^\\/]+$', file_path)
+    
+    if match:
+        return match.group(0)
+    else:
+        #If the file path is just the file name (in the root folder)
+        return file_path
